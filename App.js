332b125da54335cb847471aa04c9b9bc35d9cd35
xin ying <xinyingtheng@gmail.com>
2024-08-17 01:31:10 +0800

    notification config setting

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Platform } from 'react-native';
+import * as Notifications from 'expo-notifications';
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./navigation/AppNavigator";
 import { RootSiblingParent } from "react-native-root-siblings";
 import { Provider as LocationProvider } from "./components/LocationContext";
 import { useState, useEffect, useRef } from 'react';
+import { registerForPushNotificationsAsync, handleNotificationListeners, configureNotificationChannels } from './components/notifications';
 
 export default function App(){
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+    
+    const cleanUpListeners = handleNotificationListeners();
 
+    configureNotificationChannels();
+
+    // Clean up listeners on unmount
+    return () => {
+      cleanUpListeners();
+    };
+  }, []);
   return (
     <LocationProvider>
         <RootSiblingParent>
--- components/notifications.js
+++ components/notifications.js
@@ -1,71 +1,46 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
 import * as Notifications from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
+import { Platform } from 'react-native';
 
-export default function App() {
-  useEffect(() => {
-    registerForPushNotificationsAsync();
-    
-    const notificationListener = Notifications.addNotificationReceivedListener(notification => {
-      console.log('Notification Received:', notification);
+export async function registerForPushNotificationsAsync() {
+  let token;
+  if (Platform.OS === 'android') {
+    await Notifications.requestPermissionsAsync();
+  } else if (Platform.OS === 'ios') {
+    await Notifications.requestPermissionsAsync({
+      alert: true,
+      badge: true,
+      sound: true,
     });
-
-    const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log('Notification Response:', response);
-    });
-
-    // Clean up listeners on unmount
-    return () => {
-      notificationListener.remove();
-      responseListener.remove();
-    };
-  }, []);
-
-  async function registerForPushNotificationsAsync() {
-    let token;
-    if (Platform.OS === 'android') {
-      await Notifications.requestPermissionsAsync();
-    } else if (Platform.OS === 'ios') {
-      await Notifications.requestPermissionsAsync({
-        alert: true,
-        badge: true,
-        sound: true,
-      });
-    }
-    
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log('Expo Push Token:', token);
-    // You can send this token to your server if needed fire base
   }
-
-  async function configureChannels() {
-    if (Platform.OS === 'android') {
-      await Notifications.setNotificationChannelAsync('default', {
-        name: 'default',
-        importance: Notifications.AndroidImportance.HIGH,
-        sound: 'default',
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: '#FF231F7C',
-      });
-    }
-  }
-
-  useEffect(() => {
-    configureChannels();
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to the Notification App!</Text>
-    </View>
-  );
+  
+  token = (await Notifications.getExpoPushTokenAsync()).data;
+  console.log('Expo Push Token:', token);
+  return token;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+export function handleNotificationListeners() {
+  const notificationListener = Notifications.addNotificationReceivedListener(notification => {
+    console.log('Notification Received:', notification);
+  });
+
+  const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
+    console.log('Notification Response:', response);
+  });
+
+  return () => {
+    notificationListener.remove();
+    responseListener.remove();
+  };
+}
+
+export async function configureNotificationChannels() {
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.HIGH,
+      sound: 'default',
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+}
